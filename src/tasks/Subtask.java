11d6ad06de152d6ee39e8945c767652132665ac1
matrosov-dmitry <dmitriy.matt@ya.ru>
2025-05-25 22:27:45 +0300

    Solution Sprint-6

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -12,6 +12,11 @@ public class Subtask extends Task {
   public Subtask(String title, String description) {
     super(title, description);
   }
+  
+  public Subtask(String title, String description, int epicId) {
+    super(title, description);
+    this.epicId = epicId;
+  }
 
   public int getEpicId() {
     return epicId;
