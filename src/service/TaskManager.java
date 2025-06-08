bb55d81e1f1233b24270199f7871df827fdc8bd8
matrosov-dmitry <dmitriy.matt@ya.ru>
2025-06-09 01:22:32 +0300

    Solution Sprint 8 fix check style

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -7,14 +7,24 @@ import java.util.List;
 
 public interface TaskManager {
     void addTask(Task task);
+    
     void addSubtask(Subtask subtask);
+    
     void removeTask(int id);
+    
     void removeSubtask(int id);
+    
     void updateTask(Task task);
+    
     void updateSubtask(Subtask subtask);
+    
     Task getTask(int id);
+    
     Epic getEpic(int id);
+    
     Subtask getSubtask(int id);
+    
     List<Task> getPrioritizedTasks();
+    
     List<Subtask> getSubtasksOfEpic(int epicId);
 }
